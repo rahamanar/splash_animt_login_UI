13b194239c7fcfd6522ec55e5a4004c4b3ffaa0c
Md Hafeez <hafeez8165526@gmail.com>
2020-11-03 22:38:39 +0530

    Complete_code

---
+++ android/app/src/main/kotlin/com/example/splash_login_animt_screen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.splash_login_animt_screen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
+import 'package:splash_login_animt_screen/Animations/FadeAnimation.dart';
+import 'package:splash_login_animt_screen/LoginPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  AnimationController _scaleController;
+  AnimationController _scale2Controller;
+  AnimationController _widthController;
+  AnimationController _positionController;
+
+  Animation<double> _scaleAnimation;
+  Animation<double> _scale2Animation;
+  Animation<double> _widthAnimation;
+  Animation<double> _positionAnimation;
+
+  bool hideicon = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scaleController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _scaleAnimation =
+        Tween<double>(begin: 1.0, end: 0.8).animate(_scaleController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _widthController.forward();
+            }
+          });
+
+    _widthController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+    _widthAnimation =
+        Tween<double>(begin: 80, end: 300).animate(_widthController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              _positionController.forward();
+            }
+          });
+
+    _positionController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 1000));
+    _positionAnimation =
+        Tween<double>(begin: 0.0, end: 215).animate(_positionController)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              setState(() {
+                hideicon = true;
+              });
+              _scale2Controller.forward();
+            }
+          });
+
+    _scale2Controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+
+    _scale2Animation =
+        Tween<double>(begin: 1, end: 32).animate(_scale2Controller)
+          ..addStatusListener((status) {
+            if (status == AnimationStatus.completed) {
+              Navigator.push(
+                  context,
+                  PageTransition(
+                      child: LoginPage(), type: PageTransitionType.fade));
+            }
+          });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(3, 9, 23, 1),
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(
+                top: -50,
+                left: 0,
+                child: FadeAnimation(
+                  1,
+                  Container(
+                    width: width,
+                    height: 400,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/one.png'),
+                            fit: BoxFit.cover)),
+                  ),
+                )),
+            Positioned(
+                top: -100,
+                left: 0,
+                child: FadeAnimation(
+                  1.3,
+                  Container(
+                    width: width,
+                    height: 400,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/one.png'),
+                            fit: BoxFit.cover)),
+                  ),
+                )),
+            Positioned(
+                top: -150,
+                left: 0,
+                child: FadeAnimation(
+                  1.6,
+                  Container(
+                    width: width,
+                    height: 400,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/one.png'),
+                            fit: BoxFit.cover)),
+                  ),
+                )),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "Welcome",
+                        style: TextStyle(color: Colors.white, fontSize: 50),
+                      )),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  FadeAnimation(
+                    1.2,
+                    Text(
+                      "FLutter animated splash screen Click below to enter",
+                      style: TextStyle(
+                          color: Colors.white.withOpacity(.3), height: 1.4),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 180,
+                  ),
+                  FadeAnimation(
+                      1.4,
+                      AnimatedBuilder(
+                          animation: _scaleController,
+                          builder: (context, child) => Transform.scale(
+                                scale: _scaleAnimation.value,
+                                child: Center(
+                                  child: AnimatedBuilder(
+                                    animation: _widthController,
+                                    builder: (context, child) => Container(
+                                      width: _widthAnimation.value,
+                                      height: 80,
+                                      padding: EdgeInsets.all(10),
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(50),
+                                        color: Colors.blue.withOpacity(.4),
+                                      ),
+                                      child: InkWell(
+                                        onTap: () {
+                                          _scaleController.forward();
+                                        },
+                                        child: Stack(
+                                          children: [
+                                            AnimatedBuilder(
+                                              animation: _positionController,
+                                              builder: (context, child) =>
+                                                  Positioned(
+                                                left: _positionAnimation.value,
+                                                child: AnimatedBuilder(
+                                                  animation: _scale2Controller,
+                                                  builder: (context, child) =>
+                                                      Transform.scale(
+                                                    scale:
+                                                        _scale2Animation.value,
+                                                    child: Container(
+                                                      width: 60,
+                                                      height: 60,
+                                                      decoration: BoxDecoration(
+                                                        shape: BoxShape.circle,
+                                                        color: Colors.blue,
+                                                      ),
+                                                      child: hideicon == false
+                                                          ? Icon(
+                                                              Icons
+                                                                  .arrow_forward,
+                                                              color:
+                                                                  Colors.white,
+                                                            )
+                                                          : Container(),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ))),
+                  SizedBox(
+                    height: 60,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
